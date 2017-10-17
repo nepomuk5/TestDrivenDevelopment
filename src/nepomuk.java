7bb13ceeda3f5afa3b8f102051bdac0a8e4d0b3b
julia <julia@scz5koe-home-13.schmitz-menden.com>
2017-10-17 11:59:30 +0200

    1.Test

--- src/nepomuk.java
+++ src/nepomuk.java
@@ -6,19 +6,14 @@ public class nepomuk {
 	cocafreak simple_example, simple_cycle, complex_cycle, complex_graph;
 	@Before
 	public void setUp(){
-	simple_example = new cocafreak(
-	new String[][]{{"A", "C"}, {"C", "D"}, {"B", "C"}}
-	);
+	simple_example = new cocafreak(new String[][]{{"A", "C"}, {"C", "D"}, {"B", "C"}});
 	simple_cycle = new cocafreak(new String[][]{{"A", "A"}});
 	complex_cycle = new cocafreak(new String[][]{{"A", "B"}, {"B", "A"}});
 	complex_graph = new cocafreak(
-	new String[][]{
-	{"A", "C"}, {"C", "D"}, {"B", "C"},
-	{"A", "E"}, {"B", "F"}, {"E", "G"},
-	{"D", "G"}, {"F", "G"}, {"C", "E"},
-	{"C", "F"}
+	new String[][]{{"A", "C"}, {"C", "D"}, {"B", "C"},{"A", "E"}, {"B", "F"}, {"E", "G"},{"D", "G"}, {"F", "G"}, {"C", "E"},{"C", "F"}});
 	}
-	);
+	@Test
+	public void test1(){
+		assertFalse(simple_example.isWellSorted(new String[]{"B","D","C"}));
 	}
-
 }
