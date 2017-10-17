efe58f5e530bd99c419080f2a88d8cbacba8d9a0
cocafreak <julius.breiholz@yahoo.de>
2017-10-17 12:06:17 +0200

    update

--- src/cocafreak.java
+++ src/cocafreak.java
@@ -9,6 +9,32 @@ public class cocafreak {
 	}
 	
 	public boolean isWellSorted(String[] sequence){
+		String temp1;
+		String temp2;
+		
+			
+		for(int i = 0 ; i < m_string.length ; i++ ){
+			temp1 = m_string[i][0];
+			temp2 = m_string[i][1];
+			
+			for(int k = 0 ; k<sequence.length ; k++){
+				if(sequence[k]==temp2){
+					for(int j = 0; j<=k;){
+						if(j==k){
+							return false;
+						}
+						else if(sequence[j]!=temp1){
+							j++;
+						}else if(sequence[j]==temp1){
+							break;
+						}
+					}
+				}
+			}
+		}
+		
+		
+		
 		return true;
 }
 	
